a5aedd497a1df4f38200727ec412bcc6d57d6629
Lukas Gienapp <33417815+lgienapp@users.noreply.github.com>
2020-09-05 01:48:09 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
+function setup() {
+	# Install oh-my-zsh
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	# Install brew
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+}
+
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
@@ -12,15 +19,17 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.zsh_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	setup;
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		setup;
 		doIt;
 	fi;
 fi;
